c198102af748cc81ebfdb86253befbbb703c8c43
vishnu <vishnusathyanathan@gmail.com>
2023-09-14 15:11:47 +0530

    started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Navbar />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navbar-wrapper">
+        <div className="nav-left">Left</div>
+        <div className="nav-right">Right</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
