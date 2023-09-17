c0a99d58afb1573f8e391354b5120d9eae162aed
vishnu <vishnusathyanathan@gmail.com>
2023-09-17 23:58:53 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from "./pages/home/Home.jsx";
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Userlist from "./pages/useList/Userlist";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
       <div className="container">
         <Sidebar />
-        <Home/>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/users" element={<Userlist />} />
+        </Routes>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/useList/Userlist.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Userlist.css";
+
+const Userlist = () => {
+  return <div className="user-list">Userlist</div>;
+};
+
+export default Userlist;
